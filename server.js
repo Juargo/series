13c0210857e7c28b375b92efc1024669a84de30e
Jorge <jorge.retamal@zweicom.com>
2017-06-05 23:19:07 -0300

    mysql driver server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var mysql = require('mysql');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -17,3 +18,14 @@ app.use(express.static('public'));
 app.listen(3000, function () {
     console.log('Escuchando en 3000');
 })
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password123"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});

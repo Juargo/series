cd162219e87eaf5ad917e256025b12ec78d7c8cd
Jorge <jorge.retamal@zweicom.com>
2017-06-10 00:17:38 -0300

    router

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -1,11 +1,11 @@
 angular.module("seriesApp")
-    .controller("PrincipalCtrl", function ($scope, $http, vista) {
+    .controller("PrincipalCtrl", function ($scope, $http, vista, $routeParams) {
+        $scope.params = $routeParams;
+        console.log($scope.params);
         $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
             function (resp) {
                 vista.setvista(resp.data);
                 $scope.campos = vista.getvista();
             }
         );
-
-        $scope.tmpl = "templates/vista.html";
     })

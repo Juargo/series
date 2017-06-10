e6f25756ef442a421df6a76721640c866d08eaa0
Jorge <jorge.retamal@zweicom.com>
2017-06-10 02:34:26 -0300

    table

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -1,24 +1,23 @@
 angular.module("seriesApp")
     .controller("PrincipalCtrl", function ($scope, $http, vista, $routeParams,$timeout) {
-        // $scope.itemsByPage=5;
-        // $scope.params = $routeParams;
-        // if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
-        $timeout(function () {
+        $scope.itemsByPage=5;
+        $scope.params = $routeParams;
+        if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
+        // $timeout(function () {
         $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
             function (resp) {
-                // vista.setvista(resp.data);
-                // $scope.campos = vista.getvista();
-                $scope.gridOptions.data = resp.data;
+                vista.setvista(resp.data);
+                $scope.campos = vista.getvista();
             }
         );
-        }, 1000);
+        // }, 1000);
 
-        // $scope.campos=[
-        //      {nombre:"Kodomo"},
-        //      {nombre:"Shonen"},
-        //      {nombre:"Seinen"},
-        //      {nombre:"Josei"},
-        //      {nombre:"Cyberpunk"},
-        // ]
+        // // $scope.campos=[
+        // //      {nombre:"Kodomo"},
+        // //      {nombre:"Shonen"},
+        // //      {nombre:"Seinen"},
+        // //      {nombre:"Josei"},
+        // //      {nombre:"Cyberpunk"},
+        // // ]
 
     })

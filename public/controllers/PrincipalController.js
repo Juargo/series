132082a91d24461c92a53b8c9d3846efaf2dfe90
Jorge <jorge.retamal@zweicom.com>
2017-06-10 02:03:43 -0300

    table

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -3,11 +3,19 @@ angular.module("seriesApp")
         $scope.itemsByPage=5;
         $scope.params = $routeParams;
         if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
-        $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
-            function (resp) {
-                vista.setvista(resp.data);
-                $scope.campos = vista.getvista();
-                console.log($scope.campos);
-            }
-        );
+        // $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
+        //     function (resp) {
+        //         vista.setvista(resp.data);
+        //         $scope.campos = vista.getvista();
+        //         console.log($scope.campos);
+        //     }
+        // );
+
+        $scope.campos=[
+             {nombre:"Kodomo"},
+             {nombre:"Shonen"},
+             {nombre:"Seinen"},
+             {nombre:"Josei"},
+             {nombre:"Cyberpunk"},
+        ]
     })

8c85c2cc730575afbc9836f46290b23cb4410328
Jorge <jorge.retamal@zweicom.com>
2017-06-10 16:39:20 -0300

    router

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -1,47 +1,65 @@
 angular.module("seriesApp")
-    .controller("PrincipalCtrl", function ($scope, $http, vista, $routeParams,$timeout) {
-        $scope.itemsByPage=5;
+    .controller("PrincipalCtrl", function ($scope, $http, vista, $routeParams, $timeout) {
+        $scope.itemsByPage = 5;
         $scope.params = $routeParams;
         $scope.option = $scope.params.option;
-        if ($scope.params.option == "demografia"||$scope.params.option=="genero"|| $scope.params.option=="emisora"){ $scope.tmpl = "onecolumn.html"}
-        if ($scope.params.option == "autores"){ $scope.tmpl = "creador.html"}
-        if ($scope.params.option == "serie"){ $scope.tmpl = "serie.html"}
+        if ($scope.params.option == "demografia" || $scope.params.option == "genero" || $scope.params.option == "emisora") { $scope.tmpl = "onecolumn.html" }
+        if ($scope.params.option == "autores") { $scope.tmpl = "creador.html" }
+        if ($scope.params.option == "serie") { $scope.tmpl = "serie.html" }
         // $timeout(function () {
-        $http.get("http://174.138.52.191:3000/getquery?option="+$scope.params.option).then(
+        $http.get("http://174.138.52.191:3000/getquery?option=" + $scope.params.option).then(
             function (resp) {
                 vista.setvista(resp.data);
                 $scope.campos = vista.getvista();
             }
         );
     })
-    .controller("addCtrl",function($scope,$routeParams,$http,vista){
+    .controller("addCtrl", function ($scope, $routeParams, $http, vista) {
         $scope.params = $routeParams;
         $scope.option = $scope.params.option;
-        $http.get("http://174.138.52.191:3000/getquery?option=autores").then(
-            function (resp) {
-                vista.setvista(resp.data);
-                $scope.autores = vista.getvista();
-                console.log(resp.data);
-            }
-        );
-        if ($scope.params.option == "demografia"){ $scope.tmpl = "addDemografia.html"; $scope.table="demografia"}
-        if ($scope.params.option == "genero"){ $scope.tmpl = "addGenero.html";$scope.table="genero"}
-        if ($scope.params.option == "autores"){ $scope.tmpl = "addCreador.html"; $scope.table="creador"}
-        if ($scope.params.option == "emisora"){ $scope.tmpl = "addEmisora.html"; $scope.table="emisora"}
-        if ($scope.params.option == "serie"){ 
-            $scope.tmpl = "addSerie.html"; 
-            $scope.table="serie";
+
+        if ($scope.params.option == "demografia") { $scope.tmpl = "addDemografia.html"; $scope.table = "demografia" }
+        if ($scope.params.option == "genero") { $scope.tmpl = "addGenero.html"; $scope.table = "genero" }
+        if ($scope.params.option == "autores") { $scope.tmpl = "addCreador.html"; $scope.table = "creador" }
+        if ($scope.params.option == "emisora") { $scope.tmpl = "addEmisora.html"; $scope.table = "emisora" }
+        if ($scope.params.option == "serie") {
+            $scope.tmpl = "addSerie.html";
+            $scope.table = "serie";
+            $http.get("http://174.138.52.191:3000/getquery?option=autores").then(
+                function (resp) {
+                    vista.setvista(resp.data);
+                    $scope.autores = vista.getvista();
+                }
+            );
+            $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
+                function (resp) {
+                    vista.setvista(resp.data);
+                    $scope.demografia = vista.getvista();
+                }
+            );
+            $http.get("http://174.138.52.191:3000/getquery?option=genero").then(
+                function (resp) {
+                    vista.setvista(resp.data);
+                    $scope.genero = vista.getvista();
+                }
+            );
+            $http.get("http://174.138.52.191:3000/getquery?option=emisora").then(
+                function (resp) {
+                    vista.setvista(resp.data);
+                    $scope.emisora = vista.getvista();
+                }
+            );
         }
 
 
-        $scope.save = function(nombre, apellido){
-            if($scope.option=="demografia"|| $scope.option=="genero"|| $scope.option=="emisora"){
-                sql= "insert into " + $scope.table  +"(nombre) values ('" + nombre + "')";
+        $scope.save = function (nombre, apellido) {
+            if ($scope.option == "demografia" || $scope.option == "genero" || $scope.option == "emisora") {
+                sql = "insert into " + $scope.table + "(nombre) values ('" + nombre + "')";
             }
-            if($scope.option=="autores"){
-                sql= "insert into " + $scope.table  +"(nombre, apellido) values ('" + nombre + "','"+ apellido + "')";
+            if ($scope.option == "autores") {
+                sql = "insert into " + $scope.table + "(nombre, apellido) values ('" + nombre + "','" + apellido + "')";
             }
-            $http.post("http://174.138.52.191:3000/insert?sql="+ sql);
+            $http.post("http://174.138.52.191:3000/insert?sql=" + sql);
             $window.location.href = '/';
         }
     })

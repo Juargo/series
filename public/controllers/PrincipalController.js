e9190353e5c7a3753d455e65b5c1848e59c53eb0
Jorge <jorge.retamal@zweicom.com>
2017-06-10 01:47:21 -0300

    table

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -3,13 +3,14 @@ angular.module("seriesApp")
         $scope.itemsByPage=5;
         $scope.params = $routeParams;
         if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
-        // $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
-        //     function (resp) {
-        //         vista.setvista(resp.data);
-        //         $scope.campos = vista.getvista();
-        //         console.log($scope.campos);
-        //     }
-        // );
+        $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
+            function (resp) {
+                console.log(resp.data);
+                // vista.setvista(resp.data);
+                // $scope.campos = vista.getvista();
+                // console.log($scope.campos);
+            }
+        );
 
         $scope.myData = [
     {

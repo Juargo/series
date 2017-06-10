9d22c8522d2d9476216c4e41e35f789a8e564fb7
Jorge <jorge.retamal@zweicom.com>
2017-06-10 00:37:26 -0300

    router

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -1,5 +1,6 @@
 angular.module("seriesApp")
     .controller("PrincipalCtrl", function ($scope, $http, vista, $routeParams) {
+        scope.itemsByPage=5;
         $scope.params = $routeParams;
         if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
         $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(

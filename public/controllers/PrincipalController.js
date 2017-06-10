773efd5c4e1ef928595a527b7cb17a604e3f5705
Jorge <jorge.retamal@zweicom.com>
2017-06-10 01:43:49 -0300

    table

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -3,11 +3,34 @@ angular.module("seriesApp")
         $scope.itemsByPage=5;
         $scope.params = $routeParams;
         if ($scope.params.option == "demografia"){ $scope.tmpl = "onecolumn.html"}
-        $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
-            function (resp) {
-                vista.setvista(resp.data);
-                $scope.campos = vista.getvista();
-                console.log($scope.campos);
-            }
-        );
+        // $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
+        //     function (resp) {
+        //         vista.setvista(resp.data);
+        //         $scope.campos = vista.getvista();
+        //         console.log($scope.campos);
+        //     }
+        // );
+
+        $scope.myData = [
+    {
+        "firstName": "Cox",
+        "lastName": "Carney",
+        "company": "Enormo",
+        "employed": true
+    },
+    {
+        "firstName": "Lorraine",
+        "lastName": "Wise",
+        "company": "Comveyer",
+        "employed": false
+    },
+    {
+        "firstName": "Nancy",
+        "lastName": "Waters",
+        "company": "Fuelton",
+        "employed": false
+    }];
+
+    vista.setvista($scope.myData);
+    $scope.campos = vista.getvista();
     })

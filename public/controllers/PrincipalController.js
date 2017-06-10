c053df218609962980cb551bdf1cf88d86ccbf39
Jorge <jorge.retamal@zweicom.com>
2017-06-10 02:11:17 -0300

    table

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -6,7 +6,7 @@ angular.module("seriesApp")
         $http.get("http://174.138.52.191:3000/getquery?option=demografia").then(
             function (resp) {
                 vista.setvista(resp.data);
-                // $scope.campos = vista.getvista();
+                $scope.campos = vista.getvista();
             }
         );
         

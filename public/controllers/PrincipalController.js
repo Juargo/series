fd6c93a039a78ff3f42324c8af31d5adcf75efb6
Jorge <jorge.retamal@zweicom.com>
2017-06-11 18:09:17 -0300

    insert

--- public/controllers/PrincipalController.js
+++ public/controllers/PrincipalController.js
@@ -59,7 +59,9 @@ angular.module("seriesApp")
             if ($scope.option == "autores") {
                 sql = "insert into " + $scope.table + "(nombre, apellido) values ('" + nombre + "','" + apellido + "')";
             }
-            $http.post("http://174.138.52.191:3000/insert",sql).then(
+            dato={}
+            dato.sql = sql;
+            $http.post("http://174.138.52.191:3000/insert",dato).then(
                 // function(resp){
                 //     console.log(resp.data);
                 // }

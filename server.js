8e50421cfe42b88f475fd5db1b0c0bf1398caabf
Jorge <jorge.retamal@zweicom.com>
2017-06-11 22:30:02 -0300

    insert

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var mysql = require('mysql');
 var multer = require('multer');
+var formidable = require('formidable');
+var fs = require('fs');
 
 // var fileUpload = require('express-fileupload');
 
-
+http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.filetoupload.path;
+            var newpath = './' + files.filetoupload.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    }
+}).listen(8080);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var app = express();
@@ -18,10 +33,10 @@ var app = express();
 
 // }));
 var storage = multer.diskStorage({ //multers disk storage settings
-        destination: function (req, file, cb) {
-            cb(null, './public/')
-        }
-    });
+    destination: function (req, file, cb) {
+        cb(null, './public/')
+    }
+});
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -50,38 +65,39 @@ app.get('/getquery', function (req, res) {
     if (a == "emisora") { query = "select * from emisora" }
 
 
-        
-        con.query(query, function (err, result) {
-            if (err) throw err;
-            res.json(result);
-        });
+
+    con.query(query, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
 });
 
-app.post('/insert', function(req,res){
+app.post('/insert', function (req, res) {
     a = req.body.sql;
 
-    con.query(a, function(err,result){
-        if(err) throw err;
+    con.query(a, function (err, result) {
+        if (err) throw err;
         res.json({ status: 500, data: result.insertId });
         res.end();
     })
 })
-var upload = multer({storage: storage
-                }).single('file');
-app.post('/upload', function(req,res){
-    
-    upload(req,res,function(err){
-            if(err){
-                 console.log({error_code:1,err_desc:err});
-                 return;
-            }
-            console.log({error_code:0,err_desc:null});
-        })
-//   req.files.foo.mv('/public/'+ req.files.foo.name, function(err) {
-//     if (err)
-//       return res.status(500).send(err);
- 
-//     res.send('File uploaded!');
-//   });
+var upload = multer({
+    storage: storage
+}).single('file');
+app.post('/upload', function (req, res) {
+
+    upload(req, res, function (err) {
+        if (err) {
+            console.log({ error_code: 1, err_desc: err });
+            return;
+        }
+        console.log({ error_code: 0, err_desc: null });
+    })
+    //   req.files.foo.mv('/public/'+ req.files.foo.name, function(err) {
+    //     if (err)
+    //       return res.status(500).send(err);
+
+    //     res.send('File uploaded!');
+    //   });
 })
 
